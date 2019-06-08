0ffcb3b412d80e42bc07138485210f8604e201d4
Tochi <devkingdom@gmail.com>
2019-06-09 02:44:20 +1000

    updated unit test

--- assertionPretty/src/test/java/AssertionPrettyUnitTest.java
+++ assertionPretty/src/test/java/AssertionPrettyUnitTest.java
@@ -1,18 +1,33 @@
 import org.junit.Test;
 
 public class AssertionPrettyUnitTest {
-    public int addition (int a, int b){
+    public int badAddition(int a, int b){
         return a + b * 2;
+    }
+
+    public String getText(){
+        return "Pigs In Space - Muppet Show";
     }
 
     @Test
     public void additionTest(){
         int a = 3;
         int b = 4;
-        int testResult = addition(a, b);
+        int testResult = badAddition(a, b);
         int expectedResult = a + b;
+        boolean testCondition = testResult==expectedResult;
         AssertionPretty assertion = new AssertionPretty(testResult, expectedResult);
-        assertion.setTestDescription("Test for the addition method");
-        assertion.assertTrue(testResult==expectedResult);
+        assertion.setTestDescription("Test for the badAddition method");
+        assertion.assertTrue(testCondition);
+    }
+
+    @Test
+    public void getTextTest(){
+        String testResult = getText();
+        String expectedResult = "Pigs In Space:- Muppet Show";
+        boolean testCondition = testResult.equals(expectedResult);
+        AssertionPretty assertion = new AssertionPretty(testResult, expectedResult);
+        assertion.setTestDescription("Test the Muppet Show Title");
+        assertion.assertTrue(testCondition);
     }
 }
